f31d260df8f5ec9423538e2dd38d074c4f1d3fe1
zhukova_oksana <ksy2905@gmail.com>
2022-08-02 17:34:26 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const nameGoods = 'Сундук';
+const quantityGoods = 2;
+const categoryGoods = 'Мебель';
+const priceGoods = 50;
+
+console.log('Наименование товара:', nameGoods);
+
+console.log('Общая сумма:', quantityGoods * priceGoods);
